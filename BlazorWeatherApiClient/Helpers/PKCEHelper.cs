c8079d218bce51f8905174bc9ba514f3741219b8
Sergi Ortiz Gómez <drualcman@msn.com>
2021-04-18 00:00:45 +0800

    add blazor client

---
+++ BlazorWeatherApiClient/Helpers/PKCEHelper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BlazorWeatherApiClient.Helpers
+{
+    public class PKCEHelper
+    {
+        public static string Code_Verifier;
+        public static string Code_Challenge;
+        public const string Code_Challenge_Method = "S256";
+
+        public static void GenerateCodes()
+        {
+            byte[] RandomBytes = new byte[43];
+
+            var RandomGenerator = RandomNumberGenerator.Create();
+            RandomGenerator.GetNonZeroBytes(RandomBytes);
+
+            Code_Verifier = Base64UrlEncoder.Encode(RandomBytes);
+
+            using (var Sha256 = SHA256.Create())
+            {
+                var ChallengeBytes =
+                    Sha256.ComputeHash(Encoding.UTF8.GetBytes(Code_Verifier));
+                Code_Challenge = Base64UrlEncoder.Encode(ChallengeBytes);
+            }
+        }
+    }
+}
+
+
---
+++ BlazorWeatherApiClient/Pages/Index.razor.cs
@@ -0,0 +1,55 @@
+﻿using BlazorWeatherApiClient.Helpers;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Configuration;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWeatherApiClient.Pages
+{
+    public partial class Index 
+    {
+        [Inject]
+        IConfiguration Configuration { get; set; }
+
+        [Inject]
+        NavigationManager NavigationManager { get; set; }
+
+        [Inject]
+        IJSRuntime JSRuntime { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            PKCEHelper.GenerateCodes();
+            // Guardar el valor para utilizarlo posteriormente
+            await JSRuntime.InvokeVoidAsync("sessionStorage.setItem", "cv",
+                PKCEHelper.Code_Verifier);
+        }
+        void GetTheCode()
+        {
+            const string Response_Type = "code";
+
+            string Authorization_Endpoint =
+                Configuration["OAuth:Authorization_Endpoint"];
+            string Client_Id =
+                Configuration["OAuth:Client_Id"];
+            string Redirect_Uri =
+                Configuration["OAuth:Redirect_Uri"];
+            string Scope =
+                Configuration["OAuth:Scope"];
+
+            string URL = $"{Authorization_Endpoint}?" +
+                $"response_type={Response_Type}&" +
+                $"client_id={Client_Id}&" +
+                $"redirect_uri={Redirect_Uri}&" +
+                $"scope={Scope}&" +
+                $"code_challenge={Helpers.PKCEHelper.Code_Challenge}&" +
+                $"code_challenge_method={PKCEHelper.Code_Challenge_Method}&" +
+                $"state=MyStateValue";
+
+            NavigationManager.NavigateTo(URL);
+        }
+    }
+}
